e800f6fd68fc3971ef2169da496dc10b1ceb6cd7
Anna Mucha <anna.mucha.1401@gmail.com>
2022-09-29 21:15:08 +0200

    initial set up of home page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
     <BrowserRouter>
-   
+    <NavBar />
       <Routes>
         <Route element={<Home />} path='/' exact />
         <Route element={<About />} path='/about' />
