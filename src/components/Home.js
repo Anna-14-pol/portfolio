d9dd53624ed9fffb03edcc7612b072ea41b2ea33
Anna Mucha <anna.mucha.1401@gmail.com>
2023-02-16 18:26:46 +0100

    styles fix for main page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import image from "../garden.jpg";
+import image from "../book.gif";
 
 export default function Home() {
   return (
@@ -9,9 +9,12 @@ export default function Home() {
         alt="Garden"
         className="absolute rounded-br-full rounded-tr-full md:rounded-r-none object-scale-down sm:object-cover w-fit md:w-full h-full bg-gradient-to-r from-green-600 to-slate-800"
       />
-      <section className="relative flex justify-center min-h-screen ml-4 pt-12 lg:pt-64 md:pl-32">
-        <h1 className="text-6xl text-grey-700 sm:text-green-600 font-bold cursive leading-snug home-name">
-          Hey, Gardeners!
+      <section className="relative flex justify-center min-h-screen ml-4 pt:8 max-[600px]:pt-16  ">
+        <h1 className="text-6xl text-purple-700 lg:text-purple-400 font-bold cursive leading-snug home-name">
+          Hey,
+        </h1>
+        <h1 className="text-6xl text-purple-700 font-bold cursive leading-snug home-home ">
+          Gardeners!
         </h1>
       </section>
     </main>
