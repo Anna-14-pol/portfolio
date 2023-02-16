91b8cc19dc79cba3d9e00f3e0b11018c9f453a21
Anna Mucha <anna.mucha.1401@gmail.com>
2023-02-16 19:28:52 +0100

    fix for color shades

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
         className="absolute rounded-br-full rounded-tr-full md:rounded-r-none object-scale-down sm:object-cover w-fit md:w-full h-full bg-gradient-to-r from-green-600 to-slate-800"
       />
       <section className="relative flex justify-center min-h-screen ml-4 pt:8 max-[600px]:pt-16  ">
-        <h1 className="text-6xl text-purple-700 lg:text-purple-400 font-bold cursive leading-snug home-name">
+        <h1 className="text-6xl text-purple-600 xl:text-purple-300 font-bold cursive leading-snug home-name">
           Hey,
         </h1>
         <h1 className="text-6xl text-purple-700 font-bold cursive leading-snug home-home ">
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,21 +17,21 @@ export default function NavBar() {
           </NavLink>
           <NavLink
             to="/post"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-purple-800 font-bold hover:text-black"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-purple-800 font-bold hover:text-purple-200 "
             activeClassName="text-red-100 bg-red-700"
           >
             Garden Styles
           </NavLink>
           <NavLink
             to="/project"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-purple-800 font-bold hover:text-black"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-purple-800 font-bold hover:text-purple-200 "
             activeClassName="text-red-100 bg-red-700"
           >
             My Plants
           </NavLink>
           <NavLink
             to="/about"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-purple-800 font-bold hover:text-black"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-purple-800 font-bold hover:text-purple-200"
             activeClassName="text-red-100 bg-red-700"
           >
             About Me!
